537e0670ca0751783f7cf4241a3d003bc380f055
alexandru <alexandru356.c@gmail.com>
2024-07-19 10:55:59 -0400

    Game loop is set and the grid function is made

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <raylib.h>
-
+#include "grid.h"
 //***COMMAND LINE TO COMPILE CODE
 //g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o main
+//./main  to execute the main.exe file and run the raylib window
 //
 
 int main()
@@ -10,12 +11,19 @@ int main()
     InitWindow(300,600,"Tetris by Alexandru Ciuca");
     SetTargetFPS(60);
 
+    Grid grid = Grid();
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
+
+
+    grid.Print();
     while(WindowShouldClose() == false){
         
 
         BeginDrawing();
         ClearBackground(darkBlue);
-
+        grid.Draw();
     
 
         EndDrawing();
